29b6535f9d8c0d0825faa691985aa48828bdbe87
Wolf <wolf.wikgren@gmail.com>
2016-09-09 15:03:32 +0300

    Merge fixes

--- client/character.js
+++ client/character.js
@@ -1,50 +1,52 @@
-export default class extends PIXI.Container  {
-    constructor(name) {
-        super();
-        this.name = name;
-    }
-    
-    init() {
-        this.sprite = new PIXI.Sprite(PIXI.loader.resources[this.name].texture)
-        console.log(this.name, this.sprite);
-        this.addChild(this.sprite);
-        this.element = $('#gaijin-box');
-        this.currentScrollHeight = this.element.get(0).scrollHeight;
-    }
+export default class extends PIXI.Container {
+  constructor(name) {
+    super();
+    this.name = name;
+  }
 
-    scroll() {
-        let scrollHeight = this.element.get(0).scrollHeight;
-        if (scrollHeight != this.currentScrollHeight) {
-            this.element.animate({
-                scrollTop: scrollHeight
-            }, 500)
-            this.currentScrollHeight = scrollHeight;
-        }
-    }
+  init() {
+    this.sprite = new PIXI.Sprite(PIXI.loader.resources[this.name].texture)
+    console.log(this.name, this.sprite);
+    this.addChild(this.sprite);
+    this.element = $('#gaijin-box');
+    this.currentScrollHeight = this.element.get(0).scrollHeight;
+  }
 
-    show() {
+  scroll() {
+    let scrollHeight = this.element.get(0).scrollHeight;
+    if (scrollHeight != this.currentScrollHeight) {
+      this.element.animate({
+        scrollTop: scrollHeight
+      }, 500)
+      this.currentScrollHeight = scrollHeight;
     }
+  }
 
-    update() {
-    }
+  show() {}
 
-    load() {
-        console.log("Loading character: " + this.name);
-        PIXI.loader.add(this.name, 'assets/characters/' + this.name + '.png');        
-    }
+  update() {}
 
-    say(text) {
-        console.log(this.name + " Says: " + text);
-        return new Promise((resolve, reject) => {
-            $(() => {
-                this.element.typed({
-                    strings: ["<b>" + this.name + "</b>: " + text],
-                    typeSpeed: 0,
-                    showCursor: false,
-                    onStringTyped: () => {resolve()},
-                    onTypewrite: () => {this.scroll()}
-                });
-            });
+  load() {
+    console.log("Loading character: " + this.name);
+    PIXI.loader.add(this.name, 'assets/characters/' + this.name + '.png');
+  }
+
+  say(text) {
+    console.log(this.name + " Says: " + text);
+    return new Promise((resolve, reject) => {
+      $(() => {
+        this.element.typed({
+          strings: ["<b>" + this.name + "</b>: " + text],
+          typeSpeed: 0,
+          showCursor: false,
+          onStringTyped: () => {
+            resolve()
+          },
+          onTypewrite: () => {
+            this.scroll()
+          }
         });
-    }
+      });
+    });
+  }
 }
